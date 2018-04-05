71872490f6ce9151a212accc555bbb9ffe46f14f
D7703_03 <d7703_03@D7703-03ui-iMac.local>
2018-04-05 15:24:58 +0900

    add Alien Animation with Timer

--- Alien Animation with Timer/ViewController.swift
+++ Alien Animation with Timer/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+    var counter = 1
+    var myTimer = Timer()
 
+class ViewController: UIViewController {
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var imageCounter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageCounter.text = String(counter)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func play(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.doAnimation), userInfo: nil, repeats: true)
     }
-
+    
+    @IBAction func stop(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    
+    @objc func doAnimation() {
+        if counter == 5 {
+            counter = 1
+        } else {
+            counter = counter + 1
+        }
+        
+        myImageView.image = UIImage(named:"frame\(counter).png")
+        imageCounter.text = String(counter)
+    }
+    
 
 }
 
